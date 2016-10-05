41e7077fc40262991ff0b1bf2313c0a46870eebc
상현쓰월드 <Nam@NSHMacBook-Pro.local>
2016-10-05 09:30:03 +0900

    first commit

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,38 @@
+class TodoItemsController < ApplicationController
+	before_action :set_todo_list
+	before_action :set_todo_item, except: [:create]
+
+	def create
+		@todo_item = @todo_list.todo_items.create(todo_item_params)
+		redirect_to @todo_list
+	end
+
+	def destroy
+		if @todo_item.destroy
+			flash[:success] = "일정이 성공적으로 삭제외었습니다."
+		else
+			flash[:error] = "일정 삭제에 실패하였습니다."
+		end
+		redirect_to @todo_list
+	end
+
+	def complete
+		@todo_item.update_attribute(:completed_at, Time.now)
+		redirect_to @todo_list, notice: "일정작성이 완료되었습니다."
+	end
+
+	private
+
+	def set_todo_list
+		@todo_list = TodoList.find(params[:todo_list_id])
+	end
+
+	def set_todo_item
+		@todo_item = @todo_list.todo_items.find(params[:id])
+	end
+
+	def todo_item_params
+		params[:todo_item].permit(:content)
+	end
+
+end
